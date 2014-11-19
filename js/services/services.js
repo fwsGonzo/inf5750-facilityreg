3a9ae9c6918652ef05f3ed46189b742be03bbb40
martily <martily@ifi.uio.no>
2014-11-19 12:44:42 +0100

    Updated HTTP-headers

--- js/services/services.js
+++ js/services/services.js
@@ -5,8 +5,8 @@ angular.module('facilityReg.services', [
 ]).config(['$httpProvider', function ($httpProvider) {
 
     //Define default headers
-    $httpProvider.defaults.headers.common['Accept'] =
-        "application/json, text/plain, * / *";
+    $httpProvider.defaults.headers.get['Accept'] =
+        "application/json";
     $httpProvider.defaults.headers.post['Content-Type'] =
          "application/json";
     $httpProvider.defaults.headers.put['Content-Type'] =
