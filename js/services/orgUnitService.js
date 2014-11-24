e0e7ac4f1ae3352d7c085e9b4e1b88157528c35b
muriasen <muriasen@gmail.com>
2014-11-24 12:25:03 +0100

    Fixed conflicts

--- js/services/orgUnitService.js
+++ js/services/orgUnitService.js
@@ -1,10 +1,5 @@
 'use strict';
 
-<<<<<<< HEAD
-angular.module('facilityReg.services').factory('orgUnitService', function($resource) {
-   return $resource(dhisAPI + 'organisationUnits/', {page:'@page'});
-});
-=======
 angular.module('facilityReg.services')
   .factory('orgUnitService',
     function($resource)
@@ -17,4 +12,3 @@ angular.module('facilityReg.services')
     }
   );
 
->>>>>>> 44f96117fcc1f0ab106203337bd98a9f25627c06
